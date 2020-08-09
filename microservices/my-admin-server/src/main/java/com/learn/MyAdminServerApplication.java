e5d1cf57513c50abff393ecda56ec65723b7723a
shubham saurav <shubham.saurav96@gmail.com>
2020-08-09 14:38:05 +0530

    admin server

---
+++ microservices/my-admin-server/src/main/java/com/learn/MyAdminServerApplication.java
@@ -0,0 +1,16 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MyAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAdminServerApplication.class, args);
+	}
+
+}
